e8f1df5a89be7d53886364429e3ddb13c0f3ed23
Oliver Le <lehaoduong.vn@gmail.com>
2023-07-25 15:00:59 +0700

    meta-tag

---
+++ my-app/src/Nav.js
@@ -0,0 +1,16 @@
+
+function Nav() {
+return (
+    <div>
+        <img src="icons_assets/Logo .svg" alt = 'logo'> </img>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservation</li>
+            <li>Order Online</li>
+            <li>Login</li>
+        </ul>
+    </div>
+)
+}

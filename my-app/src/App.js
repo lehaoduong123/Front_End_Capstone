616094dd6eedfe81ac7a89ea3ddbb01429227f23
Oliver Le <lehaoduong.vn@gmail.com>
2023-08-02 08:40:37 +0700

    version1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { Route, Routes } from ' react-router-dom';
+import pages from './utils/pages';
+import Layout from './components/layout/Layout';
+import Home from './components/pages/Home';
+import Bookings from './components/pages/Bookings';
+import ConfirmedBooking from './components/pages/Bookings/ConfirmedBooking';
+import NotFound from './components/pages/NotFound';
+import UnderConstruction from './components/pages/UnderConstruction';
 
 function App() {
   return (
-    <div className="App">
-      <div>
-        <h1>My React App</h1>
-        <p>My first React app</p>
-      </div>
-    </div>
+    <Layout>
+      <Routes>
+        <Route path= {pages.get('home').path} element={<Home />} />
+        <Route path= {pages.get('bookings').path} element={<Bookings/>} />
+        <Route path= {pages.get('confirmedBooking').path} element={<ConfirmedBooking/>} />
+        <Route path= {pages.get('orderOnline').path} element={<UnderConstruction/>} />
+        <Route path= {pages.get('login').path} element={<UnderConstruction/>} />
+        <Route path="*" element={<NotFound/>} />
+      </Routes>
+    </Layout>
   );
 }
 
